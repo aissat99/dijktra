c2c3df82cfc393c0e23c8643fbb5e7482de98a03
aissat <rjulietassia@gmail.com>
2024-05-06 21:11:01 +0300

    Update: reorganizing files, and added export via PDF feature

---
+++ assets/scripts/graph.js
@@ -0,0 +1,232 @@
+// a node class
+class Node {
+    constructor(nodeName="Node", shapeImage="assets/img/server.png") {
+        // var stagePos = stage.getAbsolutePosition();
+        this.name = nodeName;
+        this.services=[];
+        this.iconPath=shapeImage;
+        this.gui = new Konva.Group({
+            // x: 30,
+            // y: 30/* + stage.height() / 2,*/
+        });
+        this.shape = new Konva.Image({
+            x:0,
+            y:20,
+            width: 30,
+            height: 40
+        });
+        this.display_name = new Konva.Text({
+            x:0,
+            y:0,
+            text: nodeName,
+            fontSize: text_style.size,
+            fontStyle: text_style.style,
+            fontFamily: text_style.font_family,
+            fill: text_style.color,
+        })
+    // properties
+        // this.gui.add(this.shape);
+        this.gui.add(this.display_name);
+        layer.add(this.gui);
+        this.shape.draggable(true);
+
+    }
+}
+
+class Edge {
+    constructor(id, node1, node2, weight){
+        this.name = [node1.name, node2.name];
+        this.id = id;
+        this.nodes = [node1, node2];
+        this.weight = weight;
+        this.shape = new Konva.Line({
+            points: [node1.shape.x()+node1.shape.width()+5, node1.shape.y()+node1.shape.height()/2, node2.shape.x()-5, node2.shape.y()+node2.shape.height()/2],
+            strokeWidth: 4,
+            stroke: 'black'
+        });
+        this.display_weight = new Konva.Text({
+            x: node1.shape.x()+40,
+            y: node1.shape.y() - 30,
+            text: this.weight,
+            fontSize: text_style.size,
+            fontStyle: text_style.style,
+            fontFamily: text_style.font_family,
+            fill: text_style.color,
+        })
+        // linking the edge to both groups, such that modification of either group will affect the edge
+        layer.add(this.shape);
+        layer.add(this.display_weight);
+    }
+}
+
+
+/// operations
+/// add a node to the graph
+function addNode(event)
+{
+    event.preventDefault();
+    deleteMode.value = false;
+    linkMode.value = false;
+    let node_name = document.getElementById('node_name').value;
+    let new_node = new Node(node_name);
+    var img = new Image();
+    img.onload= function(){
+        new_node.shape.image(img);
+        new_node.gui.add(new_node.shape);
+    }
+    img.src = new_node.iconPath;
+    nodes.push(new_node);
+    // console.log(nodes);
+    new_node.shape.on('dragmove', function() {
+        // constrain the position of the element withn the stage
+        constrainObjectPosition(new_node.shape);
+        // position of the text
+        new_node.display_name.position({
+            x: new_node.shape.x(), // Offset by 10 pixels horizontally
+            y: new_node.shape.y() - 20  // Offset by 30 pixels vertically
+        });
+        // position of the edges
+        updateEdgePositions();
+        layer.batchDraw();
+    });
+    // events on the node
+    new_node.gui.on('click', function(event){
+        //console.log(new_node.name);
+        //console.log(deleteMode.value);
+        if(deleteMode.value === true)
+        {
+            var name = new_node.name;
+            deleteNode(nodes, name);
+            deleteEdge(edges, name);
+            linkMode.value = false;
+            // if not in selecting mode, unselect any node to prevent unintended links to be created
+            selectedNode1 = null;
+            selectedNode2 = null;
+            //console.log(nodes)
+        }
+        else if(linkMode.value === true)
+        {
+            element = document.getElementById("weight_form");
+            // console.log(element.style.display);
+            if(selectedNode1 === null)
+            {
+                selectedNode1 = new_node;
+            }
+            else
+            {
+                selectedNode2 = new_node;
+                showAddForm("weight_prompt");
+                // exit the link mode
+                linkMode.value = false;
+            }
+        }
+        else
+        {
+            // if not in selecting mode, unselect any node to prevent unintended links to be created
+            selectedNode1 = null;
+            selectedNode2 = null;
+            selectedServer = new_node;
+            showAddForm("add_url_prompt");
+            var title = "Services available in: "+new_node.name;
+            document.getElementById("server_title").innerText=title;
+            updateUrlList(selectedServer);
+        }
+    });
+    // removing the form when a node is added
+    var element = document.getElementById('add_node_prompt');
+    element.style.display = 'none';
+}
+
+// function that fetches all the url in a node and gives it to the front
+function updateUrlList(server)
+{
+    // refresh the list of services in the front
+    var p = document.getElementById("url_list");
+    p.innerHTML = '';
+    var url_list = server.services;
+    for(var i = 0; i < url_list.length; i++)
+    {
+        p.innerHTML += url_list[i]+'<br /><br />';
+    }
+}
+// add an url to a server
+function addUrl(event, url)
+{
+    event.preventDefault();
+    deleteMode.value = false;
+    linkMode.value = false;
+    element = document.getElementById("add_url_prompt");
+    element.style.display = 'none';
+    // refresh the list of urls of the selected server
+    selectedServer.services.push(url);
+    selectedServer = null;
+}
+
+/// Link 2 nodes
+function linkNodes(node1, node2, weight)
+{
+    deleteMode.value = false;
+    linkMode.value = false;
+    // link the nodes with an edge
+    var edge = new Edge(node1.name+"_"+node2.name, node1, node2, weight);
+    edges.push(edge);
+    // gui
+    edge.shape.on('click', function(){
+        if(deleteMode.value === true)
+        {
+            deleteEdge(edges, edge.id);
+        }
+    });
+    // prompting the edge weight form
+    layer.draw();
+}
+
+function addWeight(event)
+{
+    event.preventDefault();
+    deleteMode.value = false;
+    linkMode.value = false;
+    element = document.getElementById("weight_prompt");
+    element.style.display = 'none';
+    var weight_value = Number(document.getElementById("weight").value);
+    linkNodes(selectedNode1, selectedNode2, weight_value);
+    // restore the values for a future writing
+    selectedNode1 = null;
+    selectedNode2 = null;
+}
+
+/// delete nodes
+function deleteNode(obj, value)
+{
+    for(var i =0; i < obj.length; i++)
+    {
+        if(obj[i].name === value)
+        {
+            console.log("found "+value+" in the nodes");
+            obj[i].gui.destroy();
+            obj.splice(i, 1); // removing the node from the graph
+            i--;
+        }
+    }
+    deleteMode.value = false;
+    linkMode.value = false;
+}
+
+/// delete edges
+function deleteEdge(obj, value)
+{
+    var i = 0;
+    for(i = 0; i < obj.length; i++)
+    {
+        if(obj[i].id.indexOf(value) !== -1)
+        {
+            console.log("found "+value+" in the edges");
+            obj[i].shape.destroy();
+            obj[i].display_weight.destroy();
+            obj.splice(i, 1); // removing the edge from the graph
+            i--;
+        }
+    }
+    deleteMode.value = false;
+    linkMode.value = false;
+}
---
+++ assets/scripts/main.js
@@ -0,0 +1,185 @@
+// global indicators
+var deleteMode = {
+    value: false
+};
+var linkMode = {
+    value: false
+};
+// the graph
+let nodes = [];
+let edges = [];
+// Get the list of font faces currently available
+var fontList = document.fonts;
+
+// console.log(nodes);
+// when in link mode, these will contain the selected nodes
+var selectedNode1 = null;
+var selectedNode2 = null;
+// for servers
+var selectedServer=null;
+
+function dijkstra(start_point, url_target)
+{
+    let visitedNodes = [];
+    let unvisitedNodes=[];
+    let distances = {};
+    let predecessors = {};
+
+    // Initialization
+    for (let i = 0; i < nodes.length; i++) {
+        let node = nodes[i].name;
+        distances[node] = node === start_point ? 0 : Infinity;
+        predecessors[node] = '';
+        unvisitedNodes.push(node);
+    }
+    console.log(distances);
+    console.log(predecessors);
+
+    // shortest distance search
+    while(unvisitedNodes.length > 0)
+    {
+        // sorting the object to see which node to mark as visited next
+        unvisitedNodes.sort((a, b) => distances[a] - distances[b]);
+
+        // getting the next node and its neighbours
+        let node = unvisitedNodes[0];
+        let node_neighb = findNeighbhours(node);
+
+        // updating the distances of the neighbours from the startting node
+        for(var i = 0; i < node_neighb.length; i++)
+        {
+            let neighb = node_neighb[i][0];
+            let neighb_dist = node_neighb[i][1];
+            let tmp_dist = distances[node] + neighb_dist;  // tmp_dist = dist(startPoint, node) + dist(node, neighb)
+            if(tmp_dist < distances[neighb])
+            {
+                distances[neighb] = tmp_dist;
+                predecessors[neighb] = node;
+            }
+        }
+
+        // marking the node as visited and thus removing it from the unvisited
+        visitedNodes.push(node);
+        unvisitedNodes.shift();
+    }
+
+    // printing the result
+    console.log("----path----")
+    console.log(predecessors);
+    console.log("----dist----")
+    console.log(distances);
+
+    // updating the UI
+    let end_point = searchPath(url_target, distances);
+    buildPath(start_point, end_point, predecessors);    // this will color the edges
+
+    // exporting the result
+    print();
+}
+
+// function that looks for all the neighbours of a given node
+function findNeighbhours(nodeName)
+{
+    let neighb = [];
+    for(var i = 0; i < edges.length; i++)
+    {
+        // if the ID of an edge contains the name of a node, then the other part is a neighbour
+        if(edges[i].id.indexOf(nodeName) !== -1)
+        {
+            // find the other part
+            if(edges[i].nodes[0].name === nodeName) // if it is the first node which has the name, then take the other node, and vice versa
+            {
+                let obj = [edges[i].nodes[1].name, edges[i].weight];
+                neighb.push(obj);
+            }
+            else
+            {
+                let obj = [edges[i].nodes[0].name, edges[i].weight];
+                neighb.push(obj);
+            }
+        }
+    }
+    return neighb;
+}
+
+function findLink(url, node)
+{
+    if (node.services.includes(url))
+    {
+        return true;
+    }
+}
+
+// function that searches for the node of a certain name
+function findNode(nodeName)
+{
+    for(var i = 0; i < nodes.length; i++)
+    {
+        if(nodes[i].name === nodeName)
+        {
+            return nodes[i];
+        }
+    }
+}
+
+function searchPath(target, paths)
+{
+    var shortestDistance = Infinity;
+    let goalNode = null;
+    // loop over the paths to look for the link
+    for(let server in paths)
+    {
+        // getting the node
+        let node = findNode(server);
+        // then see if the link belongs to the server
+        if(findLink(target, node))
+        {
+            // comparing distances to see if a shorter path is available
+            let dist = paths[server];
+            if(dist < shortestDistance)
+            {
+                shortestDistance = dist;
+                goalNode = server;
+            }
+        }
+    }
+    return goalNode;
+}
+
+function buildPath(start_point, end_point, predecessors)
+{
+    let edgesIDs = [];
+    let predecessor = end_point;
+    while(predecessor !== start_point)
+    {
+        // adding an edge to color
+        let id1 = predecessor+'_'+predecessors[predecessor];
+        let id2 = predecessors[predecessor]+'_'+predecessor;    // needed in case the id is written the other way
+        edgesIDs.push(id1);
+        edgesIDs.push(id2);
+        // going backward until reaching the start_point
+        predecessor = predecessors[predecessor];
+    }
+    console.log(edgesIDs);
+    // style: coloring the node
+    let node = findNode(start_point);
+    node.display_name.fill(highlight_color);
+    // style: coloring the edges
+    for(var i = 0; i < edges.length; i++)
+    {
+        var id = edges[i].id;
+        var edge = edges[i];
+        if(edgesIDs.includes(id))
+        {
+            console.log("done");
+            edge.shape.stroke(highlight_color);
+            edge.display_weight.fill(highlight_color);
+        }
+        else
+        {
+            edge.shape.stroke(text_style.color);
+            edge.display_weight.fill(text_style.color);
+        }
+    }
+    layer.draw();
+}
