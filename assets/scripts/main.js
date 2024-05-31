720694f5d863c8151536297293495210f0b8d957
aissat <rjulietassia@gmail.com>
2024-05-31 05:03:49 +0300

    [Update] modified the output of dijkstra to be the forwarding table of the start_point. Removed searchPath() since no more relevant, modified buildPath() to store nextHop

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,10 +24,13 @@ function dijkstra(start_point, url_target)
     let unvisitedNodes=[];
     let distances = {};
     let predecessors = {};
+    let forwardingTable = {};   // the output
 
-    // Initialization
+    // Initialization: discpvering the graph and initializing the distances from the startibg node to the other nodes
     for (let i = 0; i < nodes.length; i++) {
         let node = nodes[i].name;
+        // filling the forwarding table
+        forwardingTable[node] = '';
         distances[node] = node === start_point ? 0 : Infinity;
         predecessors[node] = '';
         unvisitedNodes.push(node);
@@ -69,10 +72,10 @@ function dijkstra(start_point, url_target)
     console.log("----dist----")
     console.log(distances);
 
-    // updating the UI
-    let end_point = searchPath(url_target, distances);
-    let nextHop = buildPath(start_point, end_point, predecessors);    // this will color the edges
-    return nextHop;
+    // building the forwarding table
+    buildTable(start_point, predecessors, forwardingTable);
+    console.log(forwardingTable);
+    return forwardingTable;
 }
 
 // function that looks for all the neighbours of a given node
@@ -100,11 +103,53 @@ function findNeighbhours(nodeName)
     return neighb;
 }
 
+// function that builds the forwarding table of a node given the table of predecessors
+function buildTable(start_point, predecessors, forwardingTable)
+{
+    let forwardingTable = {}
+    for(var i = 0; i < nodes.length; i++)
+    {
+        let end_point = nodes[i].name;
+        let nextHop = buildPath(start_point, end_point, predecessors);
+        // modifying the output port of the start_point no reach end_node
+        forwardingTable.end_point = nextHop;
+    }
+}
+
+// function that looks for the next node to go to in order to reach a specific node
+function buildPath(start_point, end_point, predecessors)
+{
+    let edgesIDs = [];
+    let predecessor = end_point;
+    let nextHop = end_point;
+    // finding the path from the end_point to the start_point
+    while(predecessor !== start_point)
+    {
+        // adding an edge to color
+        let id1 = predecessor+'_'+predecessors[predecessor];
+        let id2 = predecessors[predecessor]+'_'+predecessor;    // needed in case the id is written the other way
+        edgesIDs.push(id1);
+        edgesIDs.push(id2);
+        // storing the next node
+        nextHop = predecessor;
+        // going backward until reaching the start_point
+        predecessor = predecessors[predecessor];
+    }
+    
+    // returning the nextHop to reach end_point
+    return nextHop;
+}
+
+// function that verifies if a service is available in a server
 function findLink(url, node)
 {
     if (node.services.includes(url))
     {
         return true;
+    }
+    else
+    {
+        return false;
     }
 }
 
@@ -118,78 +163,4 @@ function findNode(nodeName)
             return i;
         }
     }
-}
-
-function searchPath(target, paths)
-{
-    var shortestDistance = Infinity;
-    let goalNode = null;
-    // loop over the paths to look for the link
-    for(let server in paths)
-    {
-        // getting the node
-        let index = findNode(server);
-        let node = nodes[index];
-        // then see if the link belongs to the server
-        if(findLink(target, node))
-        {
-            // comparing distances to see if a shorter path is available
-            let dist = paths[server];
-            if(dist < shortestDistance)
-            {
-                shortestDistance = dist;
-                goalNode = server;
-            }
-        }
-    }
-    return goalNode;
-}
-
-function buildPath(start_point, end_point, predecessors)
-{
-    let edgesIDs = [];
-    let predecessor = end_point;
-    while(predecessor !== start_point)
-    {
-        // adding an edge to color
-        let id1 = predecessor+'_'+predecessors[predecessor];
-        let id2 = predecessors[predecessor]+'_'+predecessor;    // needed in case the id is written the other way
-        edgesIDs.push(id1);
-        edgesIDs.push(id2);
-        // going backward until reaching the start_point
-        predecessor = predecessors[predecessor];
-    }
-    console.log(edgesIDs);
-    // style: coloring the node
-    for(var i = 0; i < nodes.length; i++)
-    {
-        let node = nodes[i];
-        if(nodes[i].name === start_point)
-        {
-            node.display_name.fill(highlight_color);
-        }
-        else
-        {
-            node.display_name.fill(text_style.color);
-        }
-    }
-    
-    // style: coloring the edges
-    for(var i = 0; i < edges.length; i++)
-    {
-        var id = edges[i].id;
-        var edge = edges[i];
-        if(edgesIDs.includes(id))
-        {
-            console.log("done");
-            edge.shape.stroke(highlight_color);
-            edge.display_weight.fill(highlight_color);
-        }
-        else
-        {
-            edge.shape.stroke(text_style.color);
-            edge.display_weight.fill(text_style.color);
-        }
-    }
-    layer.draw();
 }
